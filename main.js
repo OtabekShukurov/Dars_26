f10a4c7e5db9387683d38d7294123a11f2dc45d9
OtabekShukurov <otabekshukurov7535@gmail.com>
2021-04-23 08:19:05 +0500

    push origin

---
+++ main.js
@@ -0,0 +1,24 @@
+// let words = ["salom", "dunyo"]
+// let key = "Salom"
+
+// function search(words, key) {
+//     let result = []
+//     for(let item of words) {
+//         if(key.toLowerCase() === item.toLowerCase()){
+//             return true
+//         }
+
+//         return false
+//     }
+    
+//     console.log(result);
+// }
+
+let array = 1,2,3,[4,5[6,7]];
+
+function yoyvor(array) {
+    let result = []
+    for(let item of words) {
+        
+    }
+}
